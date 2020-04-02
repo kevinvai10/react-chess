f518b7c7b009e812d303c5e744aef4ae73224311
kevinvai10 <kevin.cbdev@gmail.com>
2020-04-02 01:22:39 -0700

    added basic rules and board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
+import Board from './components/ChessBoard/Chessboard';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Board} />
       </Router>
     </div>
   );
