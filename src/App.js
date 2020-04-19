4c6b7d5696d0378ab17264bbdb38be47c7ce74a6
kevinvai10 <kevin.cbdev@gmail.com>
2020-04-19 02:38:50 -0700

    added columns and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Dashboard from './pages/dashboard/Dashboard';
-import Board from './components/ChessBoard/Chessboard';
-
+import Dashboard from './pages/Dashboard/Dashboard';
+// import Board from './components/ChessBoard/Chessboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Board} />
+        <Route exact path="/" component={Dashboard} />
       </Router>
     </div>
   );
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,48 +1,84 @@
 import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
-import UserInfo from '../../components/UserInfo/UserInfo';
-import GamesInfo from '../../components/GamesInfo/GamesInfo';
+import {
+    TableContainer,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Paper
+} from '@material-ui/core';
+// UserInfo from '../../components/UserInfo/UserInfo'; import GamesInfo from
+// '../../components/GamesInfo/GamesInfo';
 import './Dashboard.scss';
 
-const Dashboard = () => (
-  <div className="Dashboard">
-    <Grid container>
-        <Grid
-            container
-            item 
-            direction="column"
-            xs={4}
-            spacing={1}
-            alignContent="center"
-            justify="center"
-        >
-            <UserInfo />
-        </Grid>
-        <Grid
-            container
-            item
-            direction="column"
-            xs={6}
-            spacing={1}
-            alignContent="center"
-            justify="center"
-        >
-            <GamesInfo />
-        </Grid>
-        <Grid
-            container
-            item
-            direction="column"
-            xs={2}
-            spacing={1}
-            alignContent="center"
-            justify="center"
-        >
-            Here goes everything
-        </Grid>
+const DEFAULT_IMAGE = 'https://www.vippng.com/png/full/416-4161690_empty-profile-picture-blank-avatar-i' +
+        'mage-circle.png';
 
-    </Grid>
-  </div>
+const createData = (name, turn, color) => ({name, turn, color});
+
+const rows = [
+    createData('Joe', 'Kevin', 'Black'),
+    createData('Jon', 'Jon', 'White'),
+    createData('Joel', 'Kevin', 'White')
+];
+const Dashboard = () => (
+    <div className="Dashboard-Container">
+        <section className="Left-Column">
+
+            <Paper className="Dashboard-Usercard">
+                <p>Name: Kevin</p>
+                <p>Score: 500</p>
+            </Paper>
+            <h4>Your Active Games</h4>
+            <TableContainer component={Paper} className="Dashboard-ActiveGames">
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>User</TableCell>
+                            <TableCell align="left">Turn</TableCell>
+                            <TableCell align="left">Color</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name}>
+                                <TableCell align="left" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="left">{row.turn}</TableCell>
+                                <TableCell align="left">{row.color}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </section>
+        <section className="Center-Column">
+            <TableContainer component={Paper} className="Dashboard-UserList">
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">User</TableCell>
+                            <TableCell align="center">Turn</TableCell>
+                            <TableCell align="center">Color</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name}>
+                                <TableCell align="center" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="center">{row.turn}</TableCell>
+                                <TableCell align="center">{row.color}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </section>
+    </div>
 );
 
 export default Dashboard;
